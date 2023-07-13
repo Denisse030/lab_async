f9722e32fc434ab5fee8dedbb0415d59d4f851d1
Denisse <96150584+Denisse030@users.noreply.github.com>
2023-07-13 14:00:00 -0400

    lab_async

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { count } = require("console");
+
 const { readFile } = require("fs").promises;
 
 const mostFrequentWord = (text) => {
@@ -14,7 +16,26 @@ const mostFrequentWord = (text) => {
 };
 
 const findPassword = async () => {
+
   // Your code goes here
+  try {
+    const poem1 = await readFile("poems/starting-poem.txt", "utf-8");
+    const poem2 = await readFile("poems/happy.txt", "utf-8");
+    const poem3 = await readFile("poems/you.txt", "utf-8");
+
+
+    const poem2FileName = `poems/${mostFrequentWord(poem1)}.txt`;
+    const poem3FileName = `poems/${mostFrequentWord(poem2)}.txt`;
+    const poem4FileName = `poems/${mostFrequentWord(poem3)}.txt`;
+
+    if (poem4FileName) {
+      console.log("most frequented word: ", poem4FileName);
+      console.log(poem4FileName);
+    }
+  } catch (error) {
+    console.log("Something went wrong when loading poem1:", error.stack);
+    return;
+  }
 };
 
 findPassword();
